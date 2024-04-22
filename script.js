1e09290a36853a19c3c8dfce77478127b21abdfa
sintijaal <112856515+sintijaal@users.noreply.github.com>
2024-04-22 15:30:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //console.log(1)
+    let zina= document.querySelector('.zina');
+    let zinas= document.querySelector('.cataZina');
+
 function sutitZinu()
 {
     console.log('sutitZinu()darbojas');
-    let zina= document.querySelector('zina');
-    let zinas= document.querySelector('cataZina');
     zinas.innerHTML = zinas.innerHTML+ '<br>'+zina.value;
+}
+function ieladetChataZinas()
+{
+    let datiNoServera= await fetch();
 }
