d0ab6b7ddb74cadb0f43ff74a092c5859ad2e174
sintijaal <112856515+sintijaal@users.noreply.github.com>
2024-04-29 14:45:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,6 @@ async function ieladetChataZinas()
 {
     let datiNoServera = await fetch('chatazinas.txt');
     let dati = await datiNoServera.text;
-    console.log(dati);
+    //console.log(dati);
+    zinas.innerHTML = dati;
 }
